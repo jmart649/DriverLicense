2bef12dda5fd530d20be97548630012aea00962c
Jesus Martinez <jmart649@stu.southtexascollege.edu>
2021-06-21 23:05:37 -0500

    Add project files.

---
+++ DriverLicense/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DriverLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test ta = new Test();
+            ta.testing();
+        }
+    }
+}
---
+++ DriverLicense/Test.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriverLicense
+{
+    class Test
+    {
+        List<string> TestAnswers = new List<string>() { "B", "D", "A", "A", "C", "A", "B", "A", "C", "D", "B", "C", "D", "A", "D", "C", "C", "B", "D", "A" };
+
+        List<string> answers = new List<string>();
+
+        public int right = 0;
+        public int wrong = 0;
+
+        public void testing()
+        {
+            for(int i = 0; i < TestAnswers.Count; i++)
+            {
+                Console.Write("Question {0}: ", i + 1);
+                string value = Console.ReadLine();
+                answers.Add(value);
+                if (value == "A" || value == "B" || value == "C" || value == "D")
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid answer!!!");
+                    break;
+                }
+            }
+
+            Console.Write("\nYou got the following question numbers wrong: ");
+            for (int i = 0; i < TestAnswers.Count; i++)
+            {
+                if (TestAnswers[i] == answers[i])
+                {
+                    right++;
+                }
+                else
+                {
+                    wrong++;
+                    Console.Write("{0}, ", i + 1);
+                }
+            }
+
+            if (right >= 15)
+            {
+                Console.WriteLine("\nYou Passed the test!!!!!");
+            }
+            else
+            {
+                Console.WriteLine("\nYou Failed the test");
+            }
+
+            Console.Write("\nTotal right answers: {0}\n", right);
+            Console.Write("Total wrong answers: {0}\n", wrong);
+        }
+    }
+        
+}
